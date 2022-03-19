466f6317b75666e90f69988eb07dd392fb41980c
SanthoshM25 <54148035+SanthoshM25@users.noreply.github.com>
2022-03-19 23:34:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import Typography from "@mui/material/Typography";
+import { events } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {events.map((event, index) => {
+        return (
+          <Timeline position="alternate">
+            {index % 2 == 0 ? (
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ m: "auto 0" }}
+                  align="right"
+                  variant="body2"
+                  color="text.secondary"
+                >
+                  {event.date}
+                  <Typography>{event.time}</Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector sx={{ bgcolor: "warning.main" }} />
+                  <TimelineDot
+                    color="secondary"
+                    variant="outlined"
+                  ></TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent sx={{ py: "20px", px: 2 }}>
+                  <Typography variant="h6" component="span">
+                    {event.title.toUpperCase()}
+                  </Typography>
+                </TimelineContent>
+              </TimelineItem>
+            ) : (
+              <TimelineItem>
+                <TimelineOppositeContent
+                  sx={{ py: "20px", px: 2 }}
+                  variant="body2"
+                >
+                  <Typography variant="h6" component="span">
+                    {event.title.toUpperCase()}
+                  </Typography>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+                  <TimelineDot color="primary" variant="filled"></TimelineDot>
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent sx={{ m: "auto 0" }} color="text.secondary">
+                  {event.date}
+                  <Typography>{event.time}</Typography>
+                </TimelineContent>
+              </TimelineItem>
+            )}
+          </Timeline>
+        );
+      })}
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const events = [
+  {
+    title: "Optimuz",
+    time: "9:00 - 10:00 AM",
+    date: "March 25",
+  },
+  {
+    title: "Meant to meet",
+    time: "10:00 - 11:30 AM",
+    date: "March 25",
+  },
+  {
+    title: "Mind Fest",
+    time: "11:30 - 12:30 AM",
+    date: "March 25",
+  },
+  {
+    title: "Riddle Quest",
+    time: "12:00 - 1:00 PM",
+    date: "March 25",
+  },
+  {
+    title: "Logo Quiz ",
+    time: "1:00 - 2:30 PM",
+    date: "March 25",
+  },
+  {
+    title: "Webinar on 5g",
+    time: "3:00 - 5:00 PM",
+    date: "March 25",
+  },
+  {
+    title: "0xdebug ",
+    time: "6:00 - 7:00 PM",
+    date: "March 25",
+  },
+  {
+    title: "Valorant",
+    time: "7:00 - 10:00 PM",
+    date: "March 25",
+  },
+  {
+    title: "BGMI",
+    time: "7:00 - 8:00 PM",
+    date: "March 25",
+  },
+  {
+    title: "COD",
+    time: "8:00 - 10:00 PM",
+    date: "March 25",
+  },
+];
